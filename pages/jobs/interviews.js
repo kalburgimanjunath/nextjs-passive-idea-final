951c34133c61142a29924d07759c96997ac95696
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-25 12:59:51 +0530

    1 file modified

--- pages/jobs/interviews.js
+++ pages/jobs/interviews.js
@@ -1,4 +1,22 @@
 import Link from 'next/link';
 export default function Interviews() {
-  return <div>Job Seeker</div>;
+  return (
+    <div>
+      <h3>Interview Preps</h3>
+      <h5>Videos</h5>
+      <ul>
+        <li>
+          <a href="https://www.youtube.com/channel/UCcdCkJKXlRoXVD03eo-q8mQ/videos">
+            Jackson Gabbard
+          </a>
+          <a href="https://www.metacareers.com/careers/SWE-L-prep-initial">
+            SWE
+          </a>
+          <a href="https://www.metacareers.com/profile/trial/?redirect=search&chooseView=Behavioral+journaling+interview">
+            Engineering Manager
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }

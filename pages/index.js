65df2bdad4d0ba9444b25ec1f9b7b5cb3aadaa8e
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-25 12:14:54 +0530

    3 files modified

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ export default function Index() {
   return (
     <div>
       {/* <EarthoOneProvider clientId="F82I0MnkpHyTHggIUkPK"> */}
+
       <Home posts={posts} users={users} />
       {/* </EarthoOneProvider> */}
     </div>

556c205f48b37a9954f483b0be0f2ec99e65570b
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-25 13:15:18 +0530

    1 file modified

--- pages/jobs/interviews.js
+++ pages/jobs/interviews.js
@@ -9,11 +9,49 @@ export default function Interviews() {
           <a href="https://www.youtube.com/channel/UCcdCkJKXlRoXVD03eo-q8mQ/videos">
             Jackson Gabbard
           </a>
+        </li>
+        <li>
           <a href="https://www.metacareers.com/careers/SWE-L-prep-initial">
             SWE
           </a>
+        </li>
+        <li>
           <a href="https://www.metacareers.com/profile/trial/?redirect=search&chooseView=Behavioral+journaling+interview">
             Engineering Manager
+          </a>
+        </li>
+        <li>
+          <a href="https://www.eventbrite.com/o/facebook-london-master-your-fb-interview-8037667565">
+            Event bite
+          </a>
+        </li>
+        <li>
+          <a href="https://www.sitepoint.com/5-typical-javascript-interview-exercises/">
+            5 Typical JavaScript Interview Exercises:{' '}
+          </a>
+        </li>
+        <li>
+          <a href="https://www.careercup.com/page?pid=facebook-interview-questions">
+            https://www.careercup.com/page?pid=facebook-interview-questions
+          </a>
+        </li>
+        <li>
+          <a href="https://leetcode.com/problemset/all/">
+            https://leetcode.com/problemset/all/
+          </a>
+        </li>
+        <li>
+          <a href="https://www.hackerrank.com/test/61sq9qfa63d/06a0a3f1a21d17dbad04a24fb285b5ce">
+            https://www.hackerrank.com/test/61sq9qfa63d/06a0a3f1a21d17dbad04a24fb285b5ce
+          </a>
+        </li>
+        <li>
+          Video Technical interview in the following link.
+          <a href="https://fb.me/1Fwn1Sg4sHC24FP">
+            https://fb.me/1Fwn1Sg4sHC24FP
+          </a>
+          <a href="https://fb.me/1Fwn1Sg4sHC24FP">
+            https://fb.me/1Fwn1Sg4sHC24FP
           </a>
         </li>
       </ul>
